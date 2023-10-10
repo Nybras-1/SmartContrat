ec5114b083931ba84fc1910701d43c9f59079a3e
Nybras-1 <andreslds1105-@live.com>
2023-10-09 21:56:29 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const ALCHEMY_API_KEY = "VGJ-6WuDF0_iZpw4xQZmFjXoNVy7IzfQ";
-const SEPOLIA_PRIVATE_KEY = "df5cbe0889b12a764798059945f4e7e1c5ba9867ce5c999c782d91bf97d1c07f";
+const ALCHEMY_API_KEY = "Ingresar su propio API JEY de ALCHEMY";
+const SEPOLIA_PRIVATE_KEY = "Ingresar clave pricada de la cuenta a utilizar";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
   etherscan: {
-    apiKey: "7IWTRK9FKI1FEZK2TEEI3URCMA5YE5UQK5",
+    apiKey: "Ingresar API KEY generada, desde la cuenta de EtherScan",
   },
   networks: {
     sepolia: {
